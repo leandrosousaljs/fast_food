98b3e1918164088dbc2253787e3644920447796c
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2025-10-06 22:55:50 -0300

    feat: enhance styling and layout with Tailwind CSS utilities and clsx integration

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,40 @@
-import { Text, View } from "react-native";
+import cn from 'clsx';
+import React, { Fragment } from 'react';
+import { FlatList, Image, Pressable, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { offers } from '@/constants';
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">Welcome to Nativewind!</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+
+          return (
+            <View>
+              <Pressable
+                className={cn('offer-card', isEven ? 'flex-row-reverse' : 'flex-row')}
+                style={{ backgroundColor: item.color }}
+              >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className={'h-full w-1/2'}>
+                      <Image source={item.image} className={'size-full'} resizeMode={'contain'} />
+                    </View>
+
+                    <View className="offer-card__info">
+                      <Text className="h1-bold text-white leading-tight">{item.title}</Text>
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
